d5d3e88a643026bb59b274e679444c3f9c84d1a8
RedBeanN <me@hongshn.xyz>
2020-07-01 09:06:52 +0800

    init dir

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 #!/usr/bin/env node
+const { version } = require('./package.json');
+const fs = require('fs');
+const path = require('path');
+const { program } = require('commander');
 
-console.log('Hello MiraiOK');
+program.version(version);
+
+program
+.command(`create [dir]`)
+.option('-t, --ts', 'Use mirai-ts')
+.description('Create a Node Mirai One Key project')
+.action((dir, cmd) => {
+  const rootDir = path.resolve(process.cwd(), dir);
+  console.log(`Creating Node Mirai project`);
+  console.log(`Root dir: ${rootDir}`);
+  if (fs.existsSync((rootDir))) console.warn(`[WARN] root directory is already exist`);
+  else fs.mkdirSync(rootDir, { recursive: true });
+});
+
+program.parse(process.argv);
