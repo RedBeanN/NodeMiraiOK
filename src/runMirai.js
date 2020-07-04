f6a500c84f707c0076764ef21c2c069a8de5f671
RedBeanN <me@hongshn.xyz>
2020-07-05 00:48:44 +0800

    add msg

--- src/runMirai.js
+++ src/runMirai.js
@@ -4,7 +4,7 @@ const runMirai = async (jar, javaPath = 'java', cmds = []) => {
   const mirai = spawn(javaPath, [`-Dfile.encoding=UTF-8`, `-jar`, jar, `--update`, `KEEP`]);
   mirai.stdout.on(`data`, data => {
     data = data.toString().trim();
-    console.log(data);
+    console.log(`[Mirai Console]`, data);
     if ((data.includes(`Mirai-console 启动完成`))) {
       for (let cmd of cmds) {
         mirai.stdin.write(`login ${cmd}\n`);
@@ -12,7 +12,7 @@ const runMirai = async (jar, javaPath = 'java', cmds = []) => {
     }
   });
   mirai.stderr.on(`data`, data => {
-    console.error(`[Error]`, data.toString())
+    console.error(`[Error]`, data.toString().trim())
   });
   mirai.on('error', (...args) => {
     console.error(`无法启动 mirai-console. 错误信息:`, ...args);
