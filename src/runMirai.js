9651ac201d752c882b77e84e660d54ac74a48100
RedBeanN <me@hongshn.xyz>
2020-07-05 00:21:02 +0800

    add login cmd

--- src/runMirai.js
+++ src/runMirai.js
@@ -7,7 +7,7 @@ const runMirai = async (jar, javaPath = 'java', cmds = []) => {
     console.log(data);
     if ((data.includes(`Mirai-console 启动完成`))) {
       for (let cmd of cmds) {
-        mirai.stdin.write(`${cmd}\n`);
+        mirai.stdin.write(`login ${cmd}\n`);
       }
     }
   });
